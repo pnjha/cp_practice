02ecd2f11bb28a20c22b1f2ebe8e1dbee4678470
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2019-08-01 12:57:43 +0530

    update

---
+++ src/cses.fi/Additional Problems/Number_Grid.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	int x,y;
+
+	cin>>x>>y;
+
+	cout<<(x-1+y-1)<<"\n";
+
+	return 0;
+}
--- src/cses.fi/string/word_combinations.cpp
+++ src/cses.fi/string/word_combinations.cpp
@@ -1,46 +1,95 @@
 #include<bits/stdc++.h>
-
+#define MOD 1000000007
 using namespace std;
 
 unordered_map<string,int> dict;
-unordered_map<int, int> memo;
 
-int dfs(string& str,int index){
+// unordered_map<int, long long int> memo;
 
-	if(index == str.length()){
-		return 1;
-	}
+// int dfs(string& str,int index){
+
+// 	if(index == str.length()){
+// 		return 1;
+// 	}
 	
-	if(memo.find(index)!=memo.end()){	
-		return memo[index];
-	}
+// 	if(memo.find(index)!=memo.end()){	
+// 		return memo[index];
+// 	}
 
-	string temp;
-	int t=0,cnt=0;
+// 	string temp;
+// 	long long int t=0,cnt=0;
 
-	for(int i = index;i<str.length();i++){
+// 	for(int i = index;i<str.length();i++){
 
-		temp = str.substr(index,i+1-index);
+// 		temp = str.substr(index,i+1-index);
 
-		cout<<"dfs temp: "<<temp<<"\n";
+// 		if(dict.find(temp)!=dict.end()){
 
-		if(dict.find(temp)!=dict.end()){
-			t = dfs(str,i+1);
-			memo[i+1] = t;
-			cnt += t;
-			cout<<"t: "<<t<<" cnt: "<<cnt<<"\n";
-		}
-	}	
+// 			if(memo.find(i+1)!=memo.end()){	
+			
+// 				cnt = (cnt + memo[i+1])%MOD;
+			
+// 			}else{
 
-	memo[index] = cnt;
+// 				cnt = (cnt + dfs(str,i+1))%MOD;
+// 			}
+// 		}
+// 	}	
 
-	return cnt;
-}
+// 	memo[index] = cnt;
+
+// 	return cnt;
+// }
+// int main(){
+
+// 	freopen("input.txt","r",stdin);
+// 	// freopen("output.txt","w",stdout);
+
+// 	int clk = clock();
+
+// 	ios_base::sync_with_stdio(NULL);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+
+// 	string str,temp;
+// 	cin>>str;
+
+// 	long long int n,cnt=0,t=0;
+// 	cin>>n;
+
+// 	for(int i = 0;i<n;i++){
+// 		cin>>temp;
+// 		dict[temp] = true;
+// 	}
+
+// 	for(int i = 0;i<str.length();i++){
+
+// 		temp = str.substr(0,i+1);
+
+// 		if(dict.find(temp)!=dict.end()){
+// 			if(memo.find(i+1)!=memo.end()){	
+			
+// 				t = memo[i+1];
+			
+// 			}else{
+
+// 				t = dfs(str,i+1);
+// 			}
+// 			cnt = (cnt+t)%MOD;
+// 		}
+// 	}
+
+// 	cout<<cnt<<"\n";
+
+// 	cout<<"Time: "<<(clock() - clk)<<"\n";
+
+// 	return 0;
+// }
 
 int main(){
 
 	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
+	// freopen("output.txt","w",stdout);
 
 	int clk = clock();
 
@@ -51,7 +100,7 @@ int main(){
 	string str,temp;
 	cin>>str;
 
-	int n,cnt=0,t=0;
+	long long int n,cnt=0,t=0;
 	cin>>n;
 
 	for(int i = 0;i<n;i++){
@@ -59,21 +108,40 @@ int main(){
 		dict[temp] = true;
 	}
 
+	vector<vector<int>> dp(str.length(),vector<int>(str.length(),0));
+
 	for(int i = 0;i<str.length();i++){
 
-		temp = str.substr(0,i+1);
+		for(int j = i+1;j<str.length();j++){
 
-		cout<<"temp: "<<temp<<"\n";
+			temp = str.substr(i,j-i-1);
 
-		if(dict.find(temp)!=dict.end()){
-			t = dfs(str,i+1);
-			// memo[i+1] = t;
-			cnt += t;
-			cout<<"t: "<<t<<" cnt: "<<cnt<<"\n";
+			if(dict.find(temp)!=dict.end()){
+
+				dp[i][j] = (dp[i][j] + 1)%MOD;
+
+			}	
 		}
 	}
 
-	cout<<cnt<<"\n";
+	int ans = 1;
+	// bool flag = false;
+
+	for(int i = 0;i<str.length();i++){
+
+		cnt = 0;
+
+		for(int j = 0;j<str.length();j++){
+
+			cnt = (cnt + dp[j][i])%MOD;
+
+		}
+
+		if(cnt>0)
+			ans = (ans * cnt)%MOD;
+	}	
+
+	cout<<ans<<"\n";
 
 	cout<<"Time: "<<(clock() - clk)<<"\n";
 
