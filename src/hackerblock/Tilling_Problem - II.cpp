49cc3afbc439538073a67adbf3d3b0ef6b59492b
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2019-08-03 12:19:21 +0530

    update

---
+++ src/hackerblock/Tilling_Problem - II.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+int tilling_dp(int n,int m){
+
+	vector<int> dp(n+1,1);
+
+	for(int i = m;i < n+1;i++){
+
+		dp[i] = dp[i-1] + dp[i-m];		
+	}
+
+	return dp.back()%MOD;
+}
+
+int main(){
+
+	int t,n,m;
+	cin>>t;
+
+	while(t--){
+
+		cin>>n>>m;
+
+		cout<<tilling_dp(n,m)%MOD<<"\n";
+
+	}
+
+
+	return 0;
+}
